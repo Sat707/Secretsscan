70c96e57bf4c7d80531d05788bb7138d37af6d87
Romain Jouhannet <romain.jouhannet@gitguardian.com>
2025-08-25 18:14:25 +0200

    Remove AWS secret

--- bucket_s3.py
+++ bucket_s3.py
@@ -5,12 +5,6 @@ from typing import Dict
 import aws_lib
 import pymongo
 
-
-def aws_upload(data: Dict):
-    database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
-    database.push(data)
-
-
 def transform_data(es_data: Dict) -> Dict:
     es_data = {**data, "origin": "ES"}
 
